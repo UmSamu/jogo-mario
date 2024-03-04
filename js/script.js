144e47fa16985dab42dee4cab3835f0ee0bee99a
UmSamu <samuca.augusto05@gmail.com>
2024-03-04 12:28:41 -0300

    Adicionando detalhes na colisão

--- js/script.js
+++ js/script.js
@@ -13,9 +13,18 @@ const loop = setInterval(() => {
     const pipePosition = pipe.offsetLeft;
     const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
     
-    if (pipePosition > 0 && pipePosition <= 120 && marioPosition < 80) {  
+    if (pipePosition > 0 && pipePosition <= 140 && marioPosition < 80) {  
         pipe.style.animation = 'none';
         pipe.style.left = `${pipePosition}px`;
+
+        mario.style.animation = 'none';
+        mario.style.bottom = `${marioPosition}px`;
+
+        mario.src = `images/game-over.png`
+        mario.style.width = '75px';
+        mario.style.marginLeft = '50px' 
+
+        clearInterval(loop);
     }
 },10)
 
